5df447746aa72e1bf13b75fb171695e080e38656
olivierbonnet <33326369+olivierbonnet@users.noreply.github.com>
2017-11-02 19:36:19 +0100

    màj des méthodes + attributs

--- hidoop/src/ordo/Job.java
+++ hidoop/src/ordo/Job.java
@@ -1,66 +1,86 @@
 package ordo;
 
 import map.MapReduce;
+import formats.Format;
 
 public class Job implements JobInterface {
 
+	private int numberOfReduces;
+	private int numberOfMaps;
+	private Format.Type inputFormat;
+	private Format.Type outputFormat;
+	private String inputFname;
+	private String outputFname;
+	private SortComparator sortComparator;
+	
+	
 	public void setNumberOfReduces(int tasks) {
-		// TODO Auto-generated method stub
-
+		this.numberOfReduces = tasks;
 	}
 
 	
 	public void setNumberOfMaps(int tasks) {
-		// TODO Auto-generated method stub
-
+		this.numberOfMaps = tasks;
 	}
 
 	
-	public void setInputFname(String fname) {
-		// TODO Auto-generated method stub
+	public void setInputFormat(Format.Type ft) {
+		this.inputFormat = ft;
+	}
 
+	
+	public void setOutputFormat(Format.Type ft) {
+		this.outputFormat = ft;
+	}
+	
+	
+	public void setInputFname(String fname) {
+		this.inputFname = fname;
 	}
 
 	
 	public void setOutputFname(String fname) {
-		// TODO Auto-generated method stub
-
+		this.outputFname = fname;
 	}
 
 	
 	public void setSortComparator(SortComparator sc) {
-		// TODO Auto-generated method stub
-
+		this.sortComparator = sc;
 	}
 
 	
 	public int getNumberOfReduces() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.numberOfReduces;
 	}
 
 	
 	public int getNumberOfMaps() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.numberOfMaps;
 	}
 
 	
+	public Format.Type getInputFormat() {
+		return this.inputFormat;
+	}
+
+	
+	public Format.Type getOutputFormat() {
+		return this.outputFormat;
+	}
+	
+	
 	public String getInputFname() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.inputFname;
 	}
 
 	
 	public String getOutputFname() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.outputFname;
 	}
 
 	
 	public SortComparator getSortComparator() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.sortComparator;
 	}
 
 	
