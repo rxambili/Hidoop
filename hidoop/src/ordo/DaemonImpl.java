518852efda5bb512b36a0f21812f3b75b363deb3
rxambili <32817521+rxambili@users.noreply.github.com>
2017-11-04 15:11:06 +0100

    update DaemonImpl

--- hidoop/src/ordo/DaemonImpl.java
+++ hidoop/src/ordo/DaemonImpl.java
@@ -3,6 +3,8 @@ package ordo;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 import map.Mapper;
@@ -26,6 +28,11 @@ public class DaemonImpl extends UnicastRemoteObject implements Daemon {
 	}
 
 	public static void main(String args[]) {
+		try {
+			Registry registry = LocateRegistry.createRegistry(4000);
+		} catch (RemoteException e1) {
+			e1.printStackTrace();
+		}
 		try {
 			Daemon daemon = new DaemonImpl();
 			Naming.rebind("//localhost:4000/Daemon" + args[0], daemon);
